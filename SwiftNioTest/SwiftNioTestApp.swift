fb63eb9a34bba39df47ac0dae1ec85702a5c8665
Damiaan Twelker <datwelk@me.com>
2023-03-22 18:19:03 +0100

    Initial Commit

---
+++ SwiftNioTest/SwiftNioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNioTestApp.swift
+//  SwiftNioTest
+//
+//  Created by Damiaan Twelker on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
